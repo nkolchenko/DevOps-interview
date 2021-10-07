b031584f8194c4a389c1b5de7aaf29ebadbaae03
Nikolay Kolchenko <nkolchenko@gmail.com>
2021-10-07 21:23:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Hello TROLOLO World!');
+    res.send('Hello TROLOLO World!!!!');
     console.log('New request arrived for: ' +req.originalUrl )
   })
 
